f39e567b36940d70a5d00b7038e403271f3a84a1
eduardogil11 <70300067+eduardogil11@users.noreply.github.com>
2020-12-03 09:59:31 -0600

    Add files via upload

---
+++ Actividad Integral 6.- Una nueva estructura/estructura.h
@@ -0,0 +1,42 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 03/12/2020
+ *      Author: Eduardo Rodríguez Gil
+ */
+#ifndef ESTRUCTURA_H
+#define ESTRUCTURA_H
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <cstring>
+#include <fstream>
+
+using namespace std;
+
+template <class T>
+class Sort {
+	public:
+  void order_data(vector<T> &r, int tam, vector<string> &m);
+};
+
+template <class T>
+void Sort<T>::order_data(vector<T> &r, int tam, vector<string> &m) {
+	for (int gap=tam/2; gap>0; gap/=2) { 
+        for (int i=gap; i<tam; i+=1) { 
+            int temp = r[i];
+            string temp2= m[i];
+            int j;   
+            for (j=i; j >= gap&&r[j-gap] > temp; j -= gap) {
+            r[j] = r[j-gap]; 
+            m[j]=m[j-gap];
+			}          
+            r[j]=temp; 
+            m[j]=temp2;			 
+        } 
+    }
+}
+
+#endif /*ESTRUCTURA_H*/
